886e53037599da157eecbcf7e8a7d22a0fafb109
Zorke <ferrari.work@gmail.com>
2026-01-05 23:31:54 -0300

    Initial commit

---
+++ src/main/java/com/helalferrari/pedidosComRabbitMQ/PedidosComRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.helalferrari.pedidosComRabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosComRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosComRabbitMqApplication.class, args);
+	}
+
+}
